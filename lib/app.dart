757873178a24be34e0d3ec73a4c2715e256ceb4f
Mahim123-Hassan <hmahim951@gmail.com>
2026-02-02 20:59:32 +0600

    added Forgot Button

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/screens/splash_screen.dart';
+
 class taskManagerApp extends StatelessWidget {
   const taskManagerApp({super.key});
 
@@ -8,6 +9,44 @@ class taskManagerApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         colorSchemeSeed: Colors.green,
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          fillColor: Colors.grey.shade200,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+           style: FilledButton.styleFrom(
+        fixedSize: Size.fromWidth(double.maxFinite),
+        padding: EdgeInsets.symmetric(vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(8),
+        ),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+
+      ),
+
+        )
       ),
       home: SplashScreen(),
     );
--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/widgets/screen_background.dart';
 
@@ -17,27 +18,60 @@ class _SignInScreenState extends State<SignInScreen> {
           padding: const EdgeInsets.all(24),
           child: Column(
             crossAxisAlignment: .start,
+            spacing: 8,
             children: [
+              SizedBox(height: 60),
               Text(
                 "Get Started With",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
               ),
+              SizedBox(height: 8),
               TextFormField(
                 decoration: InputDecoration(
                   hintText: "Email",
                   suffixIcon: Icon(Icons.email),
-                  filled: true,
-                  fillColor: Colors.grey.shade200,
-                  border: OutlineInputBorder(
-
-                      borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(8)
-                  ),
                 ),
               ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  suffixIcon: Icon(Icons.lock),
+                ),
+              ),
+              SizedBox(height: 8),
               FilledButton(
                 onPressed: () {},
                 child: Icon(Icons.arrow_circle_right_outlined),
+              ),
+              SizedBox(height: 24),
+              Column(
+                children: [
+                  Center(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Forgot Password"),
+                    ),
+                  ),
+                  //  SizedBox(height: 5),
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.5,
+                      ),
+                      text: "Don't have an account?",
+                      children: [
+                        TextSpan(
+                          style: TextStyle(color: Colors.green),
+                          text: "Sign Up",
+                          recognizer: TapGestureRecognizer()
+                            ..onTap
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
