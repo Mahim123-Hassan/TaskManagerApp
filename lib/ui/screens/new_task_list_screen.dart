27a0485d18a09b6243df19e859b6e56c58e2bb80
Mahim123-Hassan <hmahim951@gmail.com>
2026-02-05 22:49:19 +0600

    added task_card

--- lib/ui/screens/new_task_list_screen.dart
+++ lib/ui/screens/new_task_list_screen.dart
@@ -17,49 +17,13 @@ class _NewTaskListScreenState extends State<NewTaskListScreen> {
           SizedBox(),
           _buildTaskSumaryListView(),
           Expanded(
-            child: ListView.builder(
+            child: ListView.separated(
+              itemCount: 10,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text("Title of Task"),
-                  subtitle: Column(
-                    crossAxisAlignment: .start,
-                    children: [
-                      Text(
-                        "Description of the Task",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      Text("Date: 5 February 2026"),
-                      SizedBox(height: 8),
-                      Row(
-                        children: [
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 16,
-                                vertical: 4,
-                              ),
-
-                              child: Text(
-                                "New",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                          ),
-                          Spacer(),
-                          IconButton(onPressed: (){}, icon: Icon(Icons.delete,color: Colors.grey,)),
-                          IconButton(onPressed: (){}, icon: Icon(Icons.edit,color: Colors.grey,))
-                        ],
-                      ),
-                    ],
-                  ),
-                );
+                return TaskCard();
+              },
+              separatorBuilder: (context,index){
+                return SizedBox(height: 8,);
               },
             ),
           ),
@@ -69,33 +33,58 @@ class _NewTaskListScreenState extends State<NewTaskListScreen> {
   }
 }
 
-class _buildTaskSumaryListView extends StatelessWidget {
-  const _buildTaskSumaryListView({super.key});
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 0,
-            color: Colors.white,
-            margin: EdgeInsets.only(left: 16),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  Text("12", style: TextTheme.of(context).titleMedium),
-                  Text("New", style: TextTheme.of(context).labelSmall),
-                ],
-              ),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      child: ListTile(
+        tileColor: Colors.white,
+        title: Text("Title of Task"),
+        subtitle: Column(
+          crossAxisAlignment: .start,
+          children: [
+            Text(
+              "Description of the Task",
+              style: TextStyle(color: Colors.grey),
             ),
-          );
-        },
+            Text("Date: 5 February 2026"),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 4,
+                    ),
+
+                    child: Text(
+                      "New",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                Spacer(),
+                IconButton(onPressed: (){}, icon: Icon(Icons.delete,color: Colors.grey,)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.edit,color: Colors.grey,))
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
---
+++ lib/ui/screens/task_card.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+class _buildTaskSumaryListView extends StatelessWidget {
+  const _buildTaskSumaryListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 0,
+            color: Colors.white,
+            margin: EdgeInsets.only(left: 16),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  Text("12", style: TextTheme.of(context).titleMedium),
+                  Text("New", style: TextTheme.of(context).labelSmall),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
